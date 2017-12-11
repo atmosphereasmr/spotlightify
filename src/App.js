8ce36f54bfe7c5cf6bce9f1f08d99d1e36bf7627
Carissa Christian <atmosphereasmr@gmail.com>
2017-12-10 22:36:47 -0700

    uninstalled browser router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import "./App.css"
-import { BrowserRouter, Route } from 'react-browser-router';
 
 import Home from './Components/Home/home'
 import SearchBar from './Components/SearchBar/search-bar'
@@ -10,14 +9,12 @@ import Data from './Components/Data/data'
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
       <div>
       <SearchBar />
       <Home />
       <PicAndBio />
       <Data />
       </div>
-      </BrowserRouter>
     );
   }
 }
