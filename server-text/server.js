34545626e93359f8a22b134a1888fc7a4b02bd91
Carissa Christian <atmosphereasmr@gmail.com>
2018-04-27 13:36:30 -0600

    yes

--- server-text/server.js
+++ server-text/server.js
@@ -67,6 +67,14 @@ app.use(express.static(__dirname + '/public'))
      console.log("app running")
    })
 
+   app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../build/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+   })
+
 app.get('/login', function(req, res) {
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
