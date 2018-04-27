a21dacc6ecaf65ed784a31388d8facb54844446c
Carissa Christian <atmosphereasmr@gmail.com>
2018-04-27 13:16:26 -0600

    yes

--- server-text/server.js
+++ server-text/server.js
@@ -13,7 +13,7 @@ var bodyParser = require('body-parser');
 
 var client_id = '03a1fa81261d484d83f60c14183d0087'; // Your client id
 var client_secret = '4f2bdb7b4b9d42fcb904aa9d97f5cce5'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'http://spotlightify.herokuapp.com/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
@@ -138,14 +138,14 @@ app.get('/callback', function(req, res) {
           req.session.email = body.email
         }
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/' +
+        res.redirect('http://spotlightify.herokuapp.com/' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token,
             email: email
           }));
       } else {
-        res.redirect('http://localhost:3000/' +
+        res.redirect('http://spotlightify.herokuapp.com/' +
           querystring.stringify({
             error: 'invalid_token'
           }));
